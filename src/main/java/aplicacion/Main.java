88082e521bac90163b7b4741acf592db5c6e9e20
InflationOk05 <benjamindiazsamoluk2005@gmail.com>
2025-10-20 02:36:58 -0300

    Agregar estructura

--- src/main/java/aplicacion/Main.java
+++ src/main/java/aplicacion/Main.java
@@ -1,4 +1,14 @@
 package aplicacion;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
